5b5b9e9143d9cdf480a65f50b937e0ce25a880d2
AleksandrovaDaria <dariaaleksandrova0815@outlook.com>
2023-08-26 09:11:31 +0200

    Dodaj pliki projektów.

---
+++ ToRead.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace EisenhowerMatrixApp
+{
+    public class ToRead
+    {
+        [Name(name: "Title")]
+        public string Title { get; set; }
+
+        [Name(name: "Deadline")]
+        public DateTime Deadline { get; set; }
+
+        [Name(name: "IsDone")]
+        public bool IsDone { get; set; }
+
+    }
+}
+   
